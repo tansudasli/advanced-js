53c1d47386bd51207586d34e3a8ddc5d2adfa39a
Tansu Dasli <tansudasli@gmail.com>
2019-06-25 12:21:55 +0300

    clearity increased

--- webapi.js
+++ webapi.js
@@ -1,4 +1,6 @@
 // setTimeout is handled by wepapi engine, not the stack !
 console.log("1");
+
 setTimeout(() => {console.log("2"), 1000});  //or setTimeout(function () {console.log("2"), 1000});
+
 console.log("3");
