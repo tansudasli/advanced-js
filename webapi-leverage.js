49a5167e941102c2014a1cb489fa8427805e6c08
Tansu Dasli <tansudasli@gmail.com>
2019-06-25 12:50:25 +0300

    renamed

--- webapi-leverage.js
+++ webapi-leverage.js
@@ -1,7 +1,7 @@
 const list = new Array(6000).join('1.1').split('.');
 
 function removeItemsFromList() {
-    var item = list.pop();
+    let item = list.pop();
 
     if (item) {
         setTimeout(removeItemsFromList, 0); //instead removeItemsFromList(); to eliminate stack overflow
@@ -12,4 +12,4 @@ console.log(list.length);
 
 removeItemsFromList();
 
-console.log(list.length);
+console.log(list.length); //use chrome's javascript console, and run this line again & again
