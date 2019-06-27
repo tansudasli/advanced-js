2f96a7d5055426dfd4d3a77e3ad19c52837212b3
Tansu Dasli <tansudasli@gmail.com>
2019-06-27 19:45:42 +0300

    closures - minor update

---
+++ core/prototypal-inheritance/prototypal-inheritance.js
@@ -0,0 +1,10 @@
+const arr = [];
+
+//use chrome developer
+
+arr.__proto__.__proto__.toString();
+
+/*
+in Js, there is no classical inheritance,nor class (just a syntax).
+there is prototypal inheritance.!!
+ */
