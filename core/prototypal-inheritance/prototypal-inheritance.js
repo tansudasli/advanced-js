ce3f22df1c8389ba2ce5d912922cb36ca8f3e399
Tansu Dasli <tansudasli@gmail.com>
2019-06-27 22:03:47 +0300

    closures - minor update

--- core/prototypal-inheritance/prototypal-inheritance.js
+++ core/prototypal-inheritance/prototypal-inheritance.js
@@ -8,6 +8,6 @@ arr.__proto__.__proto__.toString();
 in Js, there is no classical inheritance,nor class (just a syntax).
 there is prototypal inheritance.!!
 
-- manually assiging [lizard.__proto__ = dragon;] is not performing well!
+- manually assiging [lizard.__proto__ = dragon;] is not performing well! use Object.create() instead.
 - js does not copy inherited properties. it looks through the prototypal chain! very efficient !!
  */
