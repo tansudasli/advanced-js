884bd65313476aac80d0c62e94311c4ff8731aed
Tansu Dasli <tansudasli@gmail.com>
2019-06-28 14:35:16 +0300

    functional programming - partial application vs currying

--- core/functional-programming/concepts/currying.js
+++ core/functional-programming/concepts/currying.js
@@ -1,12 +1,17 @@
+//normal fn
 const multiply = (a, b) => a * b;
 
 const r = multiply(2,3);
 console.log(r);
 
-//currying
+//currying = one param at a time
 const curriedMultiply = (a) => (b) => a * b;
 const curriedMultiplyByFive = curriedMultiply(5);
 
 console.log(curriedMultiply(3)(4));
 
 console.log(curriedMultiplyByFive(3));
+
+//partial application
+const partialMultiplyByFive = multiply.bind(null, 5);
+console.log(partialMultiplyByFive(10));
