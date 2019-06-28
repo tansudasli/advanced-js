984e9cfaa1f6ce77900e9560eac4611ddefd9af5
Tansu Dasli <tansudasli@gmail.com>
2019-06-28 15:05:43 +0300

    functional programming - memoization better version

---
+++ core/functional-programming/concepts/memoization-better.js
@@ -0,0 +1,23 @@
+
+//used closure to access cache outside of fn scope.
+const memoizedAddTo80 = () => {
+    let cache = {};
+
+    return function (n) {
+
+        if (n in cache)
+            return cache[n];
+        else {
+            console.log('loading....');
+
+            cache[n] = n + 80;
+            return cache[n];
+        }
+    }
+};
+
+const memoized = memoizedAddTo80();
+
+console.log(memoized(80));
+console.log(memoized(80)); //comes from cache
+console.log(memoized(80)); //comes from cache
