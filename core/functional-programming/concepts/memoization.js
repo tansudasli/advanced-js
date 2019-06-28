0b5b1dd80c5413febbcb356765f32b149734e2e0
Tansu Dasli <tansudasli@gmail.com>
2019-06-28 14:58:31 +0300

    functional programming - memoization

---
+++ core/functional-programming/concepts/memoization.js
@@ -0,0 +1,17 @@
+let cache = {};
+
+const memoizedAddTo80 = (n) => {
+    if (n in cache)
+        return cache[n];
+    else {
+        console.log('loading....');
+
+        cache[n] = n + 80;
+        return cache[n];
+    }
+};
+
+
+console.log(memoizedAddTo80(80));
+console.log(memoizedAddTo80(80)); //comes from cache
+console.log(memoizedAddTo80(80)); //comes from cache
