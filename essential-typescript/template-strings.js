cd56d92442389dd7a263d5b1d765b3984e08b346
Tansu Dasli <tansudasli@gmail.com>
2019-07-26 13:01:13 +0300

    minor update

--- essential-typescript/template-strings.js
+++ essential-typescript/template-strings.js
@@ -8,5 +8,4 @@ var toDo = [{ id: 123, name: "Pick up truck", completed: true },
     { id: 124, name: "Pick me up", completed: false },
     { id: 125, name: "Bring back truck", completed: false }
 ];
-//dont use [[ ]], `` is much better
 container.innerHTML = "\n        <ul class=\"list-group\">\n            <li id=\"" + toDo[0].id + "\" class=\"" + (toDo[0].completed ? "list-group-item  list-group-item-success" : "list-group-item active") + "\">" + toDo[0].name + "</li>\n            <li id=\"" + toDo[1].id + "\" class=\"" + (toDo[1].completed ? "list-group-item  list-group-item-success" : "list-group-item active") + "\">" + toDo[1].name + "</li>\n            <li id=\"" + toDo[2].id + "\" class=\"" + (toDo[2].completed ? "list-group-item  list-group-item-success" : "list-group-item active") + "\">" + toDo[2].name + "</li>\n        </ul>\n";
