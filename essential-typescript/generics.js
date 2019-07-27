837436d2dcf2c821ce86cf2ef7c0b66eb953cfd8
Tansu Dasli <tansudasli@gmail.com>
2019-07-27 10:59:08 +0300

    generics i typescript - explicitly set params

--- essential-typescript/generics.js
+++ essential-typescript/generics.js
@@ -9,3 +9,9 @@ var pair1 = new KeyValuePair(3, "uc");
 var pair2 = new KeyValuePair("bes", 5);
 console.log(pair1);
 console.log(pair2);
+//run w/  tsc generics.ts && node generics.js
+//explicitly define params may better for compile time. such as Date.now() actually returns a number
+var pair3 = new KeyValuePair("bes", 5);
+var pair4 = new KeyValuePair("bes", new Date(Date.now()));
+console.log(pair3);
+console.log(pair4);
--- essential-typescript/generics.ts
+++ essential-typescript/generics.ts
@@ -13,3 +13,10 @@ console.log(pair2);
 
 
 //run w/  tsc generics.ts && node generics.js
+
+//explicitly define params may better for compile time. such as Date.now() actually returns a number
+let pair3 = new KeyValuePair<string, number>("bes", 5);
+let pair4 = new KeyValuePair<string, Date>("bes", new Date(Date.now()));
+
+console.log(pair3);
+console.log(pair4);
