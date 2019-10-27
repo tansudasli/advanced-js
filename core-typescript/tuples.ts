f02162f2867a2c7aa8b248e618aa43f8c7a9486f
Tansu Dasli <tansudasli@gmail.com>
2019-10-27 11:21:32 +0300

    restructuring and clarification w/ module names

---
+++ core-typescript/tuples.ts
@@ -0,0 +1,11 @@
+// in a object format, it is easy to understand what is what
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+
+// tuple = arrays w/ type
+// suitable mostly for csv files
+// harder to understand what is what..
+const pepsi : [string, boolean, number] = ['brown', true, 50];
